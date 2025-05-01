8f0a4179311a1caab5b5a4c529b65fb7958261be
ANOUD <a20nood@gmail.com>
2025-05-01 10:33:03 +0300

    Initial Commit

---
+++ mulim/mulimApp.swift
@@ -0,0 +1,17 @@
+//
+//  mulimApp.swift
+//  mulim
+//
+//  Created by Alanoud Alamrani on 03/11/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct mulimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
