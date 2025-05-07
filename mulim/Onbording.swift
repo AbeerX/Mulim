de750b2294b0e94e2db2ff91f7782d6194ecbfbf
ANOUD <a20nood@gmail.com>
2025-05-07 10:48:48 +0300

    final edit onbording nav

--- mulim/Onbording.swift
+++ mulim/Onbording.swift
@@ -1,3 +1,106 @@
+//  Onbording.swift
+//  mulim
+//
+//  Created by Alanoud Alamrani on 06/11/1446 AH.
+//
+
+import SwiftUI
+
+struct Onbording: View {
+    @AppStorage("hasSeenOnboarding") var hasSeenOnboarding: Bool = false
+     @State private var goToProducts = false
+    var body: some View {
+
+        NavigationStack {
+            
+            
+            VStack {
+                HStack {
+                    Spacer()
+                    Text("Skip")
+                        .foregroundColor(Color("C1"))
+                        .font(.system(size: 18, weight: .bold))
+                        .padding()
+                }
+                
+                TabView {
+                    OnbordingPage(imageName: "OB1", title: "Welcome to Mulim!", description: """
+              Easily manage your bookings and track your store reports.
+              All your tools in one place to save time and focus on growing your business.
+              """)
+                    
+                    OnbordingPage(imageName: "OB2", title: "", description: """
+                Easily explore every order's details
+              everything you need is just a tap away
+              
+              """)
+                    
+                    
+                    VStack {
+                                           OnbordingPage(imageName: "OB3", title: "", description: """
+                                           Schedule order deliveries by day and track them with ease.
+                                           Browse past and upcoming orders, and organize your schedule accurately and on time.
+                                           """)
+                                           
+                                           Button(action: {
+                                               hasSeenOnboarding = true
+                                               goToProducts = true
+                                           }) {
+                                               Text("Get Started")
+                                                   .foregroundColor(.black)
+                                                   .font(.system(size: 18, weight: .bold))
+                                                   .padding()
+                                                   .frame(width: 362, height: 52)
+                                                   .background(Color("C1"))
+                                                   .cornerRadius(25)
+                                           }
+                                       }
+                    
+                }
+                
+                .tabViewStyle(PageTabViewStyle())
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            }
+        }.navigationBarBackButtonHidden(true)
+
+        
+    }
+
+    
+        struct OnbordingPage:View {
+            
+            var imageName: String
+            var title: String
+            var description: String
+            
+            var body: some View {
+                
+                VStack {
+                    Image(imageName)
+                        .frame(height: 350)
+                    Text(title)
+                        .padding(.bottom, 4)
+                    Text(description)
+                        .fontWeight(.regular)
+                        .multilineTextAlignment(.center)
+                        .font(.system(size: 14))
+                        .lineLimit(nil)
+                    
+                } .padding(.bottom, 140.0)
+                
+            }
+        }
+        
+    }
+    
+
+#Preview {
+    Onbording()
+}
+
+
+
+
 //
 //  Onbording.swift
 //  mulim
@@ -58,65 +161,67 @@
 //                            hasSeenOnboarding = true
 //                        })
 //                    }
-                    VStack {
-                        OnbordingPage(imageName: "OB3", title: "", description: """
-                        Schedule order deliveries by day and track them with ease.
-                        Browse past and upcoming orders, and organize your schedule accurately and on time.
-                        """)
-                        
-                        Button(action: {
-                            hasSeenOnboarding = true
-                            goToProducts = true
-                        }) {
-                            Text("Get Started")
-                                .foregroundColor(.black)
-                                .font(.system(size: 18, weight: .bold))
-                                .padding()
-                                .frame(width: 362, height: 52)
-                                .background(Color("C1"))
-                                .cornerRadius(25)
-                        }
-                    }
+//                    VStack {
+//                        OnbordingPage(imageName: "OB3", title: "", description: """
+//                        Schedule order deliveries by day and track them with ease.
+//                        Browse past and upcoming orders, and organize your schedule accurately and on time.
+//                        """)
+//                        
+//                        Button(action: {
+//                            hasSeenOnboarding = true
+//                            goToProducts = true
+//                        }) {
+//                            Text("Get Started")
+//                                .foregroundColor(.black)
+//                                .font(.system(size: 18, weight: .bold))
+//                                .padding()
+//                                .frame(width: 362, height: 52)
+//                                .background(Color("C1"))
+//                                .cornerRadius(25)
+//                        }
+//                    }
+//
+//                    
+//                }
+//                
+//                .tabViewStyle(PageTabViewStyle())
+//                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+//            }
+//        }.navigationBarBackButtonHidden(true)
+//
+//        
+//    }
+//
+//    
+//        struct OnbordingPage:View {
+//            
+//            var imageName: String
+//            var title: String
+//            var description: String
+//            
+//            var body: some View {
+//                
+//                VStack {
+//                    Image(imageName)
+//                        .frame(height: 350)
+//                    Text(title)
+//                        .padding(.bottom, 4)
+//                    Text(description)
+//                        .fontWeight(.regular)
+//                        .multilineTextAlignment(.center)
+//                        .font(.system(size: 14))
+//                        .lineLimit(nil)
+//                    
+//                } .padding(.bottom, 140.0)
+//                
+//            }
+//        }
+//        
+//    }
+//    
+//
+//#Preview {
+//    Onbording()
+//}
 
-                    
-                }
-                
-                .tabViewStyle(PageTabViewStyle())
-                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
-            }
-        }.navigationBarBackButtonHidden(true)
 
-        
-    }
-
-    
-        struct OnbordingPage:View {
-            
-            var imageName: String
-            var title: String
-            var description: String
-            
-            var body: some View {
-                
-                VStack {
-                    Image(imageName)
-                        .frame(height: 350)
-                    Text(title)
-                        .padding(.bottom, 4)
-                    Text(description)
-                        .fontWeight(.regular)
-                        .multilineTextAlignment(.center)
-                        .font(.system(size: 14))
-                        .lineLimit(nil)
-                    
-                } .padding(.bottom, 140.0)
-                
-            }
-        }
-        
-    }
-    
-
-#Preview {
-    Onbording()
-}
--- mulim/mulimApp.swift
+++ mulim/mulimApp.swift
@@ -7,23 +7,49 @@
 
 import SwiftUI
 import SwiftData
+
 @main
 struct mulimApp: App {
-    @AppStorage("hasCompletedFirstSetup") private var hasCompletedFirstSetup = false
+    @AppStorage("hasSeenOnboarding") var hasSeenOnboarding: Bool = false
+    @AppStorage("hasCompletedFirstSetup") var hasCompletedFirstSetup: Bool = false
+
+
     var body: some Scene {
-        
         WindowGroup {
-            if hasCompletedFirstSetup {
-                MainTabView()
+            if !hasSeenOnboarding {
+                Onbording()
+                    .environment(\.font, Font.custom("Tajawal-Regular", size: 16))
+            } else if !hasCompletedFirstSetup {
+                Products1stView {
+                    hasCompletedFirstSetup = true
+                }
+                    .environment(\.font, Font.custom("Tajawal-Regular", size: 16))
             } else {
-                Products1stView(onFinish: {
-                                 hasCompletedFirstSetup = true
-                             })
-                         }
+                MainTabView()
+                    .environment(\.font, Font.custom("Tajawal-Regular", size: 16))
+            }
         }
-        .modelContainer(for: Product.self) // ✅ مكانه الصحيح هنا
-        
-    }}
+        .modelContainer(for: Product.self)
+    }
+}
+
+//@main
+//struct mulimApp: App {
+//    @AppStorage("hasCompletedFirstSetup") private var hasCompletedFirstSetup = false
+//    var body: some Scene {
+//        
+//        WindowGroup {
+//            if hasCompletedFirstSetup {
+//                MainTabView()
+//            } else {
+//                Products1stView(onFinish: {
+//                                 hasCompletedFirstSetup = true
+//                             })
+//                         }
+//        }
+//        .modelContainer(for: Product.self) // ✅ مكانه الصحيح هنا
+//        
+//    }}
 
   /*  @AppStorage("hasSeenOnboarding") var hasSeenOnboarding: Bool = false
     var body: some Scene {
