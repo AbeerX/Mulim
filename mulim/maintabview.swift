0530457b2c6b98f1d2086b84523bcd67fbf2d5af
ANOUD <a20nood@gmail.com>
2025-05-07 09:59:29 +0300

    edit main tab view

--- mulim/maintabview.swift
+++ mulim/maintabview.swift
@@ -1,48 +1,48 @@
+////
+////  maintabview.swift
+////  mulim
+////
+////  Created by Alanoud Alamrani on 08/11/1446 AH.
+////
 //
-//  maintabview.swift
-//  mulim
+//import SwiftUI
 //
-//  Created by Alanoud Alamrani on 08/11/1446 AH.
+//struct MainTabView: View {
+//    var body: some View {
+//        NavigationStack {
+//            TabView {
+//                NavigationStack {
+//                    Main()
+//                }
+//                .tabItem {
+//                    Image(systemName: "house.fill")
+//                    Text("Main")
+//                    
+//                   
+//                }
+//                
+//                NavigationStack {
+//               
+//                }
+//                .tabItem {
+//                    Image(systemName: "cart.fill.badge.plus")
+//                        .resizable()
+//                        .frame(width: 22, height: 22)
+//                    Text("Orders")
+//                }
+//                
+//                NavigationStack {
+//                  
+//                }
+//                .tabItem {
+//                    Image(systemName: "book.pages.fill")
+//                    Text("Products")
+//                }
+//            }
+//            .accentColor(Color("C1"))
+//            .navigationBarBackButtonHidden(true)
+//        }
+//    }
+//}
 //
-
-import SwiftUI
-
-struct MainTabView: View {
-    var body: some View {
-        NavigationStack {
-            TabView {
-                NavigationStack {
-                    Main()
-                }
-                .tabItem {
-                    Image(systemName: "house.fill")
-                    Text("Main")
-                    
-                   
-                }
-                
-                NavigationStack {
-               
-                }
-                .tabItem {
-                    Image(systemName: "cart.fill.badge.plus")
-                        .resizable()
-                        .frame(width: 22, height: 22)
-                    Text("Orders")
-                }
-                
-                NavigationStack {
-                  
-                }
-                .tabItem {
-                    Image(systemName: "book.pages.fill")
-                    Text("Products")
-                }
-            }
-            .accentColor(Color("C1"))
-            .navigationBarBackButtonHidden(true)
-        }
-    }
-}
-
-
+//
--- mulim/mulimApp.swift
+++ mulim/mulimApp.swift
@@ -9,21 +9,21 @@ import SwiftUI
 import SwiftData
 @main
 struct mulimApp: App {
-    var body: some Scene {
-         WindowGroup {
-             Products1stView().modelContainer(for: Product.self)
-         }
-     }
-  /*  @AppStorage("hasSeenOnboarding") var hasSeenOnboarding: Bool = false
+    //    var body: some Scene {
+    //         WindowGroup {
+    //             Products1stView().modelContainer(for: Product.self)
+    //         }
+    //     }
+    @AppStorage("hasSeenOnboarding") var hasSeenOnboarding: Bool = false
     var body: some Scene {
         WindowGroup {
             if hasSeenOnboarding {
-                          MainTabView()
-                              .environment(\.font, Font.custom("Tajawal-Regular", size: 16))
-                      } else {
-                          Onbording()
-                              .environment(\.font, Font.custom("Tajawal-Regular", size: 16))
-                      }
+                MainTabView()
+                    .environment(\.font, Font.custom("Tajawal-Regular", size: 16))
+            } else {
+                Onbording()
+                    .environment(\.font, Font.custom("Tajawal-Regular", size: 16))
             }
-    }*/
+        }
+    }
 }
