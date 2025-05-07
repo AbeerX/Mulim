1884fcd89fd2bbaa7b3a84f0da91497205cacdcd
ANOUD <a20nood@gmail.com>
2025-05-07 10:01:02 +0300

    edit maintabview

--- mulim/maintabview.swift
+++ mulim/maintabview.swift
@@ -1,48 +1,48 @@
-////
-////  maintabview.swift
-////  mulim
-////
-////  Created by Alanoud Alamrani on 08/11/1446 AH.
-////
 //
-//import SwiftUI
+//  maintabview.swift
+//  mulim
 //
-//struct MainTabView: View {
-//    var body: some View {
-//        NavigationStack {
-//            TabView {
-//                NavigationStack {
-//                    Main()
-//                }
-//                .tabItem {
-//                    Image(systemName: "house.fill")
-//                    Text("Main")
-//                    
-//                   
-//                }
-//                
-//                NavigationStack {
-//               
-//                }
-//                .tabItem {
-//                    Image(systemName: "cart.fill.badge.plus")
-//                        .resizable()
-//                        .frame(width: 22, height: 22)
-//                    Text("Orders")
-//                }
-//                
-//                NavigationStack {
-//                  
-//                }
-//                .tabItem {
-//                    Image(systemName: "book.pages.fill")
-//                    Text("Products")
-//                }
-//            }
-//            .accentColor(Color("C1"))
-//            .navigationBarBackButtonHidden(true)
-//        }
-//    }
-//}
+//  Created by Alanoud Alamrani on 08/11/1446 AH.
 //
-//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        //        NavigationStack {
+        //            TabView {
+        //                NavigationStack {
+        //                    Main()
+        //                }
+        //                .tabItem {
+        //                    Image(systemName: "house.fill")
+        //                    Text("Main")
+        //
+        //
+        //                }
+        //
+        //                NavigationStack {
+        //
+        //                }
+        //                .tabItem {
+        //                    Image(systemName: "cart.fill.badge.plus")
+        //                        .resizable()
+        //                        .frame(width: 22, height: 22)
+        //                    Text("Orders")
+        //                }
+        //
+        //                NavigationStack {
+        //
+        //                }
+        //                .tabItem {
+        //                    Image(systemName: "book.pages.fill")
+        //                    Text("Products")
+        //                }
+        //            }
+        //            .accentColor(Color("C1"))
+        //            .navigationBarBackButtonHidden(true)
+        //        }
+        
+    }
+    
+}
